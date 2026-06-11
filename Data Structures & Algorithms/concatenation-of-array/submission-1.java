75d987d98a0a258210e4b1b9f61945f2e8bcc763
kambojmayan-png <kambojmayan@gmail.com>
2026-06-11 14:23:57 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i = 0 ; i<2*n ; i++){
+            if(i<n){
+            ans[i] = nums[i];
+            }
+            else{
+            ans[i] = nums[i-n];
+            }
+        }
+        // for(int i = 0 ; i<n ; i++){
+        //     ans[n+i] = nums[i];
+        // }
+        return ans;
+    }
+}
